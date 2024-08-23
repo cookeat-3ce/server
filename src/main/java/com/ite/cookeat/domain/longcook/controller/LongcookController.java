158a2e3e35fc7514c9c547ceec110472cc4f44d7
yangjaehyuk <gndksakxk10@naver.com>
2024-08-23 20:15:13 +0900

    :sparkles: Feat : 크리에이터 스-윽쿡 조회(9개씩)

--- src/main/java/com/ite/cookeat/domain/longcook/controller/LongcookController.java
+++ src/main/java/com/ite/cookeat/domain/longcook/controller/LongcookController.java
@@ -1,5 +1,28 @@
 package com.ite.cookeat.domain.longcook.controller;
 
+import com.ite.cookeat.domain.longcook.dto.GetLongcookReq;
+import com.ite.cookeat.domain.longcook.dto.GetLongcookRes;
+import com.ite.cookeat.domain.longcook.service.LongcookService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/longcook")
+@RequiredArgsConstructor
 public class LongcookController {
 
+  private final LongcookService longcookService;
+
+  @GetMapping("/{username}")
+  public ResponseEntity<List<GetLongcookRes>> findLongcookList(
+      @PathVariable("username") String username, @RequestParam(defaultValue = "1") Integer page) {
+    GetLongcookReq modifiedReq = GetLongcookReq.builder().username(username).page(page).build();
+    return ResponseEntity.ok(longcookService.findLongcook(modifiedReq));
+  }
 }
--- src/main/java/com/ite/cookeat/domain/longcook/service/LongcookServiceImpl.java
+++ src/main/java/com/ite/cookeat/domain/longcook/service/LongcookServiceImpl.java
@@ -1,5 +1,20 @@
 package com.ite.cookeat.domain.longcook.service;
 
+import com.ite.cookeat.domain.longcook.dto.GetLongcookReq;
+import com.ite.cookeat.domain.longcook.dto.GetLongcookRes;
+import com.ite.cookeat.domain.longcook.mapper.LongcookMapper;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class LongcookServiceImpl implements LongcookService {
 
+  private final LongcookMapper longcookMapper;
+
+  @Override
+  public List<GetLongcookRes> findLongcook(GetLongcookReq getLongcookReq) {
+    return longcookMapper.selectLongcook(getLongcookReq);
+  }
 }
