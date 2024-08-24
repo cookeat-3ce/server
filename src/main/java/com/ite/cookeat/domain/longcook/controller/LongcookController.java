ca574752a31aef0c85f245f1b38c52e3f1e1046b
김지수 <wltn2858@swu.ac.kr>
2024-08-24 18:13:19 +0900

    :adhesive_bandage: Chore : longcookcontroller URL, 메서드명 수정

--- src/main/java/com/ite/cookeat/domain/longcook/controller/LongcookController.java
+++ src/main/java/com/ite/cookeat/domain/longcook/controller/LongcookController.java
@@ -4,15 +4,13 @@ import com.ite.cookeat.domain.longcook.dto.GetLongcookReq;
 import com.ite.cookeat.domain.longcook.dto.GetLongcookRes;
 import com.ite.cookeat.domain.longcook.service.LongcookService;
 import java.util.List;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,8 +24,8 @@ public class LongcookController {
     return ResponseEntity.ok(longcookService.findLongcook(longcookId));
   }
 
-  @GetMapping("/{username}")
-  public ResponseEntity<List<GetLongcookRes>> findLongcookList(
+  @GetMapping("/list/{username}")
+  public ResponseEntity<List<GetLongcookRes>> longcookList(
       @PathVariable("username") String username, @RequestParam(defaultValue = "1") Integer page) {
     GetLongcookReq modifiedReq = GetLongcookReq.builder().username(username).page(page).build();
     return ResponseEntity.ok(longcookService.findLongcookList(modifiedReq));
