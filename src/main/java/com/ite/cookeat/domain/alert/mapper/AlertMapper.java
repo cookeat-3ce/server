a68f5f1b0043161f0f7817b99094bffd6d3601c7
김지수 <wltn2858@swu.ac.kr>
2024-08-26 03:39:28 +0900

    :sparkles: Feat : [사용자] 이벤트 알림 받기 등록

--- src/main/java/com/ite/cookeat/domain/alert/mapper/AlertMapper.java
+++ src/main/java/com/ite/cookeat/domain/alert/mapper/AlertMapper.java
@@ -1,5 +1,8 @@
 package com.ite.cookeat.domain.alert.mapper;
 
+import com.ite.cookeat.domain.alert.dto.PostEventAlertMemberReq;
+
 public interface AlertMapper {
 
+  Integer insertEventAlertMember(PostEventAlertMemberReq req);
 }
--- src/main/java/com/ite/cookeat/domain/alert/service/AlertServiceImpl.java
+++ src/main/java/com/ite/cookeat/domain/alert/service/AlertServiceImpl.java
@@ -1,5 +1,27 @@
 package com.ite.cookeat.domain.alert.service;
 
-public class AlertServiceImpl {
+import static com.ite.cookeat.exception.ErrorCode.ADD_MEMBER_TO_ALERT_CONFLICT;
 
+import com.ite.cookeat.domain.alert.dto.PostEventAlertMemberReq;
+import com.ite.cookeat.domain.alert.mapper.AlertMapper;
+import com.ite.cookeat.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AlertServiceImpl implements AlertService {
+
+  private final AlertMapper alertMapper;
+
+  @Override
+  @Transactional
+  public Integer addEventAlertMember(PostEventAlertMemberReq req) {
+    Integer result = alertMapper.insertEventAlertMember(req);
+    if (result <= 0) {
+      throw new CustomException(ADD_MEMBER_TO_ALERT_CONFLICT);
+    }
+    return result;
+  }
 }
