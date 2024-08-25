aa3c424c23b04d81d37d7c701fb782a695845098
김지수 <wltn2858@swu.ac.kr>
2024-08-25 10:50:54 +0900

    :sparkles: Feat : 이벤트 목록 조회(페이징), 상세 조회

--- src/main/java/com/ite/cookeat/domain/event/controller/EventController.java
+++ src/main/java/com/ite/cookeat/domain/event/controller/EventController.java
@@ -1,5 +1,32 @@
 package com.ite.cookeat.domain.event.controller;
 
+import com.ite.cookeat.domain.event.dto.GetEventDetailRes;
+import com.ite.cookeat.domain.event.dto.GetEventPageRes;
+import com.ite.cookeat.domain.event.service.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/event")
 public class EventController {
 
+  private final EventService eventService;
+
+  @GetMapping("/{eventId}")
+  public ResponseEntity<GetEventDetailRes> eventList(@PathVariable Integer eventId) {
+    return ResponseEntity.ok(eventService.findEventDetail(eventId));
+  }
+
+  @GetMapping
+  public ResponseEntity<GetEventPageRes> eventDetail(
+      @RequestParam(defaultValue = "1") Integer page) {
+    return ResponseEntity.ok(eventService.findEventList(page));
+  }
 }
+
