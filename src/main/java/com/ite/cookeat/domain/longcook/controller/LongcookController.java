18103cb050bc8bd7e35425c2d6f32705d8a296e4
yjpark <ox8764@naver.com>
2024-08-26 11:01:06 +0900

    :sparkles: Feat : 롱쿡 수정

--- src/main/java/com/ite/cookeat/domain/longcook/controller/LongcookController.java
+++ src/main/java/com/ite/cookeat/domain/longcook/controller/LongcookController.java
@@ -9,9 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,5 +39,12 @@ public class LongcookController {
   public ResponseEntity<?> longcookDelete(@PathVariable Integer longcookId) {
     longcookService.modifyLongcookDeletedate(longcookId);
     return ResponseEntity.noContent().build();
+  }
+
+  @PutMapping(consumes = {"multipart/form-data"})
+  public ResponseEntity<?> longcookModify(
+      @RequestPart("file") MultipartFile file,
+      @RequestPart("longcook") String request) {
+    return ResponseEntity.ok(longcookService.modifyLongcook(request, file));
   }
 }
--- src/main/java/com/ite/cookeat/domain/longcook/service/LongcookServiceImpl.java
+++ src/main/java/com/ite/cookeat/domain/longcook/service/LongcookServiceImpl.java
@@ -1,22 +1,30 @@
 package com.ite.cookeat.domain.longcook.service;
 
+import static com.ite.cookeat.exception.ErrorCode.FILE_UPLOAD_FAIL;
 import static com.ite.cookeat.exception.ErrorCode.LONGCOOK_NOT_FOUND;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ite.cookeat.domain.longcook.dto.GetLongcookReq;
 import com.ite.cookeat.domain.longcook.dto.GetLongcookRes;
+import com.ite.cookeat.domain.longcook.dto.PutLongcookReq;
 import com.ite.cookeat.domain.longcook.mapper.LongcookMapper;
 import com.ite.cookeat.exception.CustomException;
+import com.ite.cookeat.s3.service.S3UploadService;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class LongcookServiceImpl implements LongcookService {
 
   private final LongcookMapper longcookMapper;
+  private final S3UploadService s3UploadService;
+  private final ObjectMapper objectMapper;
 
   @Override
   @Transactional(readOnly = true)
@@ -39,5 +47,25 @@ public class LongcookServiceImpl implements LongcookService {
       throw new CustomException(LONGCOOK_NOT_FOUND);
     }
     return longcookId;
+  }
+
+  @Override
+  @Transactional
+  public Integer modifyLongcook(String request, MultipartFile file) {
+
+    String longcookUrl = null;
+    PutLongcookReq putLongcookReq = null;
+    try {
+      putLongcookReq = objectMapper.readValue(request, PutLongcookReq.class);
+      longcookUrl = s3UploadService.saveFile(file);
+    } catch (IOException e) {
+      throw new CustomException(FILE_UPLOAD_FAIL);
+    }
+    putLongcookReq.setLongcookUrl(longcookUrl);
+    Integer result = longcookMapper.updateLongcook(putLongcookReq);
+    if (result <= 0) {
+      throw new CustomException(LONGCOOK_NOT_FOUND);
+    }
+    return result;
   }
 }
