238a5f28a18e6503515deaea547d9b2f2e1f5dd2
김지수 <wltn2858@swu.ac.kr>
2024-09-01 00:57:22 +0900

    :adhesive_bandage: Chore : 중복 Errorcode 제거

--- src/main/java/com/ite/cookeat/exception/ErrorCode.java
+++ src/main/java/com/ite/cookeat/exception/ErrorCode.java
@@ -38,7 +38,6 @@ public enum ErrorCode {
   /* code: 409 */
   ADD_MEMBER_TO_ALERT_CONFLICT(CONFLICT, "알림받기 등록에 실패했습니다."),
   REPORTED_SSKCOOK_CONFLICT(CONFLICT, "신고된 슥쿡에 대한 삭제처리가 실패했습니다."),
-  ADD_MEMBER_TO_ALERT_CONFLICT(CONFLICT, "알림받기 등록에 실패했습니다."),
   SUBSCRIPTION_CONFLICT(CONFLICT, "구독에 실패했습니다.");
   /* code: 500 */
 
