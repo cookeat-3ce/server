4f993fe9ea43968be8034beabd4ac54c2e6a924c
yangjaehyuk <gndksakxk10@naver.com>
2024-09-02 01:02:09 +0900

    :adhesive_bandage: Chore : response에 신고 여부 추가

--- src/main/java/com/ite/cookeat/domain/sskcook/dto/GetSskcookDetailsRes.java
+++ src/main/java/com/ite/cookeat/domain/sskcook/dto/GetSskcookDetailsRes.java
@@ -15,6 +15,7 @@ public class GetSskcookDetailsRes {
   private String sskcookUrl;
   private String recipe;
   private String title;
+  private String reportStatus;
   private String likeStatus;
   private String storeStatus;
   private String followStatus;
