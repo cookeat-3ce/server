eae61f36a3e2410b57d438fee1b11c51ef566da6
김지수 <wltn2858@swu.ac.kr>
2024-09-21 20:33:03 +0900

    :adhesive_bandage: Chore : 이벤트 목록 조회 페이지 크기, filtering 적용

--- src/main/java/com/ite/cookeat/domain/event/mapper/EventMapper.java
+++ src/main/java/com/ite/cookeat/domain/event/mapper/EventMapper.java
@@ -4,10 +4,12 @@ import com.ite.cookeat.domain.event.dto.GetEventDetailRes;
 import com.ite.cookeat.domain.event.dto.GetEventRes;
 import com.ite.cookeat.global.dto.Criteria;
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface EventMapper {
 
-  List<GetEventRes> selectEventList(Criteria cri);
+  List<GetEventRes> selectEventList(@Param("cri") Criteria cri,
+      @Param("filtering") String filtering);
 
   GetEventDetailRes selectEventDetail(Integer eventId);
 
