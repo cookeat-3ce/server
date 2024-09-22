1b4dfa267a754408815da2afb116164c5f8c4fa1
yjpark <ox8764@naver.com>
2024-09-23 00:54:47 +0900

    :bulb: Comment : S3 주석 추가

--- src/main/java/com/ite/cookeat/s3/service/S3UploadService.java
+++ src/main/java/com/ite/cookeat/s3/service/S3UploadService.java
@@ -10,6 +10,20 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+/**
+ * S3 이미지 업로드 Service
+ *
+ * @author 박유진
+ * @version 1.0
+ * @since 2024.08.24
+ *
+ *
+ * <pre>
+ * 수정일          수정자         내용
+ * ------------- ----------- ---------------------------------
+ * 2024.08.24    박유진       최초 생성
+ * </pre>
+ */
 @Service
 @RequiredArgsConstructor
 public class S3UploadService {
@@ -19,7 +33,13 @@ public class S3UploadService {
   @Value("${cloud.aws.s3.bucket}")
   private String awsBucket;
 
-
+  /**
+   * 파일을 지정된 S3 버킷에 저장
+   *
+   * @param multipartFile 업로드할 파일
+   * @return 업로드된 파일의 URL
+   * @throws IOException 파일 업로드 중 입출력 오류가 발생할 경우
+   */
   public String saveFile(MultipartFile multipartFile) throws IOException {
     String originalFilename = multipartFile.getOriginalFilename();
 
@@ -31,11 +51,22 @@ public class S3UploadService {
     return amazonS3.getUrl(awsBucket, originalFilename).toString();
   }
 
+  /**
+   * S3에서 파일 다운로드
+   *
+   * @param originalFilename 다운로드할 파일의 이름
+   * @return 파일의 URL을 포함한 UrlResource 객체
+   */
   public UrlResource downloadFile(String originalFilename) {
     UrlResource urlResource = new UrlResource(amazonS3.getUrl(awsBucket, originalFilename));
     return urlResource;
   }
 
+  /**
+   * S3 버킷에서 파일을 삭제
+   *
+   * @param originalFilename 삭제할 파일의 이름
+   */
   public void deleteFile(String originalFilename) {
     amazonS3.deleteObject(awsBucket, originalFilename);
   }
